91faa0406ce91bfae8438da06b76897726487dec
AR123456 <coloradoz99@gmail.com>
2021-10-30 09:10:50 -0600

    platform construtor

--- Doodle-Jump/app.js
+++ Doodle-Jump/app.js
@@ -11,11 +11,33 @@ function createDoodler() {
   doodler.style.left = doodlerLeftSpace + "px";
   doodler.style.bottom = doodlerBottomSpace + "px";
 }
+// constructor class for platform
+class Platform {
+  constructor(newPlatBottom) {
+    // each platform will need a bottom, left spacing
+    this.bottom = newPlatBottom;
+    this.left = Math.random() * 315;
+    // create the div for the platform
+    this.visual = document.createElement("div");
+    // need to store as a variable fist since this. will not work to add class list
+
+    const visual = this.visual;
+    // add styling
+    visual.classList.add("platform");
+    visual.style.left = this.left + "px";
+    visual.style.bottom = this.bottom + "px";
+    // append the grid div with the new plaform
+    grid.appendChild(visual);
+  }
+}
 function createPlatforms() {
   for (let i = 0; i < platformCount; i++) {
     ///gap between platforms hight of grid/platformCount
     let platGap = 600 / platformCount;
+    //using for loop to increment the sizeof platform
     let newPlatBottom = 100 + i * platGap;
+    // using class here to create new platform
+    let newPlatform = new Platform();
   }
 }
 

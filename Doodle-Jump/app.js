dd350bbb96dc55376bf853f22f3b4d6831b795a3
AR123456 <coloradoz99@gmail.com>
2021-10-30 09:08:34 -0600

    doodle start game

--- Doodle-Jump/app.js
+++ Doodle-Jump/app.js
@@ -2,6 +2,8 @@ const grid = document.querySelector(".grid");
 const doodler = document.createElement("div");
 let doodlerLeftSpace = 50;
 let doodlerBottomSpace = 150;
+let isGameOver = false;
+let platformCount = 5;
 
 function createDoodler() {
   grid.appendChild(doodler);
@@ -9,4 +11,19 @@ function createDoodler() {
   doodler.style.left = doodlerLeftSpace + "px";
   doodler.style.bottom = doodlerBottomSpace + "px";
 }
-createDoodler();
+function createPlatforms() {
+  for (let i = 0; i < platformCount; i++) {
+    ///
+  }
+}
+
+function start() {
+  if (!isGameOver) {
+    //   if the game is not over create the doodler
+    createDoodler();
+    createPlatforms();
+  }
+}
+// TODO attach to button
+start();
+// restart at 13min   https://www.youtube.com/watch?v=8xPsg6yv7TU

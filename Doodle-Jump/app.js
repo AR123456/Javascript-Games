c95b236cba7cd5a608efc4a555c789ef2b30b2f0
AR123456 <coloradoz99@gmail.com>
2021-11-07 08:08:10 -0700

    move left and right

--- Doodle-Jump/app.js
+++ Doodle-Jump/app.js
@@ -103,16 +103,36 @@ function control(e) {
   if (e.key === "ArrowLeft") {
     moveLeft();
   } else if (e.key === "ArrowRight") {
-    // move right
+    moveRight();
   } else if (e.key === "ArrowUp") {
     // moveStraight up
   }
 }
 function moveLeft() {
+  console.log("left");
+  if (isGoingRight) {
+    clearInterval(rightTimerId);
+    isGoingRight = false;
+  }
   isGoingLeft = true;
   leftTimerId = setInterval(function () {
-    doodlerLeftSpace -= 5;
-    doodler.style.left = doodlerLeftSpace + "px";
+    if (doodlerLeftSpace >= 0) {
+      doodlerLeftSpace -= 5;
+      doodler.style.left = doodlerLeftSpace + "px";
+    } else moveRight();
+  }, 30);
+}
+function moveRight() {
+  if (isGoingLeft) {
+    clearInterval(leftTimerId);
+    isGoingLeft = false;
+  }
+  isGoingRight = true;
+  rightTimerId = setInterval(function () {
+    if (doodlerLeftSpace <= 340) {
+      doodlerLeftSpace += 5;
+      doodler.style.left = doodlerLeftSpace + "px";
+    } else moveLeft();
   }, 30);
 }
 function start() {

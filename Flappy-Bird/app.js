981ffb495254dbd89d144d5e39fdf43a7736f6ba
AR123456 <coloradoz99@gmail.com>
2021-11-11 07:12:05 -0700

    bird hits obstical

--- Flappy-Bird/app.js
+++ Flappy-Bird/app.js
@@ -6,7 +6,6 @@ let birdLeft = 220;
 let birdBottom = 100;
 let gravity = 2;
 let isGameOver = false;
-let gap = 530;
 // move bird to bottom center of sky div
 function startGame() {
   birdBottom -= gravity;
@@ -41,38 +40,22 @@ function generateObstacle() {
   // console.log(randomHeight);
   let obstacleBottom = randomHeight;
   const obstacle = document.createElement("div");
-  const topObstacle = document.createElement("div");
-
-  if (!isGameOver) {
-    obstacle.classList.add("obstacle");
-    topObstacle.classList.add("topObstacle");
-  }
-  gameDisplay.appendChild(obstacle);
-
-  gameDisplay.appendChild(topObstacle);
+  obstacle.classList.add("obstacle");
+  if (!isGameOver) gameDisplay.appendChild(obstacle);
   obstacle.style.left = obstacleLeft + "px";
-  topObstacle.style.left = obstacleLeft + "px";
   obstacle.style.bottom = obstacleBottom + "px";
-  topObstacle.style.bottom = obstacleBottom + "px";
-  topObstacle.style.bottom = obstacleBottom + gap + "px";
   //   moving the obstacle
   function moveObstacle() {
     obstacleLeft -= 2;
     obstacle.style.left = obstacleLeft + "px";
-    topObstacle.style.left = obstacleLeft + "px";
-
+    console.log(obstacleLeft);
     if (obstacleLeft === -60) {
       clearTimeout(timerId);
       gameDisplay.removeChild(obstacle);
-      gameDisplay.removeChild(topObstacle);
     }
     // bird colides with obstacle or bird hits ground
     if (
-      (obstacleLeft > 200 &&
-        obstacleLeft < 280 &&
-        birdLeft === 220 &&
-        (birdBottom < obstacleBottom + 153 ||
-          birdBottom > obstacleBottom + gap - 200)) ||
+      (obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220) ||
       birdBottom === 0
     ) {
       gameOver();
@@ -92,4 +75,3 @@ function gameOver() {
   isGameOver = true;
   document.removeEventListener("keyup", control);
 }
-// https://www.youtube.com/watch?v=8xPsg6yv7TU

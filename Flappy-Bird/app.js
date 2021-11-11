02f34723f994672ccdb7657201c1183314c3cd09
AR123456 <coloradoz99@gmail.com>
2021-11-11 07:12:39 -0700

    flappy bird hits pipe

--- Flappy-Bird/app.js
+++ Flappy-Bird/app.js
@@ -2,10 +2,10 @@ const bird = document.querySelector(".bird");
 const gameDisplay = document.querySelector(".game-container");
 const ground = document.querySelector(".ground");
 
-let birdLeft = 220;
+let birdLeft = 200;
 let birdBottom = 100;
 let gravity = 2;
-let isGameOver = false;
+
 // move bird to bottom center of sky div
 function startGame() {
   birdBottom -= gravity;
@@ -14,7 +14,7 @@ function startGame() {
   //
 }
 // run startGame every 20 ms
-let gameTimerId = setInterval(startGame, 20);
+let timerId = setInterval(startGame, 20);
 // use clearInterval to stop it from running
 // clearInterval(timerId);
 // jump up aginst gravity
@@ -30,48 +30,28 @@ function jump() {
   // each time the jump function is called adding 50px but not if it would cause bird to jump off page
   if (birdBottom < 500) birdBottom += 50;
   bird.style.bottom = birdBottom + "px";
-  // console.log(birdBottom);
+  console.log(birdBottom);
 }
 document.addEventListener("keyup", control);
 
 function generateObstacle() {
   let obstacleLeft = 500;
   let randomHeight = Math.random() * 60;
-  // console.log(randomHeight);
+  console.log(randomHeight);
   let obstacleBottom = randomHeight;
   const obstacle = document.createElement("div");
   obstacle.classList.add("obstacle");
-  if (!isGameOver) gameDisplay.appendChild(obstacle);
+  gameDisplay.appendChild(obstacle);
   obstacle.style.left = obstacleLeft + "px";
   obstacle.style.bottom = obstacleBottom + "px";
   //   moving the obstacle
   function moveObstacle() {
     obstacleLeft -= 2;
     obstacle.style.left = obstacleLeft + "px";
-    console.log(obstacleLeft);
-    if (obstacleLeft === -60) {
-      clearTimeout(timerId);
-      gameDisplay.removeChild(obstacle);
-    }
-    // bird colides with obstacle or bird hits ground
-    if (
-      (obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220) ||
-      birdBottom === 0
-    ) {
-      gameOver();
-      clearInterval(timerId);
-    }
   }
   // move obstacle every 20ms
   let timerId = setInterval(moveObstacle, 20);
-  // genrate more obstacles by putting generateObstacle in setTimeout
-  if (!isGameOver) setTimeout(generateObstacle, 3000);
 }
 
 generateObstacle();
-function gameOver() {
-  console.log("game over");
-  clearInterval(gameTimerId);
-  isGameOver = true;
-  document.removeEventListener("keyup", control);
-}
+// https://www.youtube.com/watch?v=8xPsg6yv7TU

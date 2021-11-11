5e2c0ed7d26db80751876b8aeb02f974b761ddff
AR123456 <coloradoz99@gmail.com>
2021-11-11 07:06:52 -0700

    flappy bird working and commented

--- Flappy-Bird/app.js
+++ Flappy-Bird/app.js
@@ -6,6 +6,7 @@ let birdLeft = 220;
 let birdBottom = 100;
 let gravity = 2;
 let isGameOver = false;
+let gap = 530;
 // move bird to bottom center of sky div
 function startGame() {
   birdBottom -= gravity;
@@ -40,22 +41,38 @@ function generateObstacle() {
   // console.log(randomHeight);
   let obstacleBottom = randomHeight;
   const obstacle = document.createElement("div");
-  obstacle.classList.add("obstacle");
-  if (!isGameOver) gameDisplay.appendChild(obstacle);
+  const topObstacle = document.createElement("div");
+
+  if (!isGameOver) {
+    obstacle.classList.add("obstacle");
+    topObstacle.classList.add("topObstacle");
+  }
+  gameDisplay.appendChild(obstacle);
+
+  gameDisplay.appendChild(topObstacle);
   obstacle.style.left = obstacleLeft + "px";
+  topObstacle.style.left = obstacleLeft + "px";
   obstacle.style.bottom = obstacleBottom + "px";
+  topObstacle.style.bottom = obstacleBottom + "px";
+  topObstacle.style.bottom = obstacleBottom + gap + "px";
   //   moving the obstacle
   function moveObstacle() {
     obstacleLeft -= 2;
     obstacle.style.left = obstacleLeft + "px";
-    console.log(obstacleLeft);
+    topObstacle.style.left = obstacleLeft + "px";
+
     if (obstacleLeft === -60) {
       clearTimeout(timerId);
       gameDisplay.removeChild(obstacle);
+      gameDisplay.removeChild(topObstacle);
     }
     // bird colides with obstacle or bird hits ground
     if (
-      (obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220) ||
+      (obstacleLeft > 200 &&
+        obstacleLeft < 280 &&
+        birdLeft === 220 &&
+        (birdBottom < obstacleBottom + 153 ||
+          birdBottom > obstacleBottom + gap - 200)) ||
       birdBottom === 0
     ) {
       gameOver();
@@ -75,3 +92,4 @@ function gameOver() {
   isGameOver = true;
   document.removeEventListener("keyup", control);
 }
+// https://www.youtube.com/watch?v=8xPsg6yv7TU

e4a59abafeb208e1688d3d5b56c79153137c5396
AR123456 <coloradoz99@gmail.com>
2021-11-11 07:13:55 -0700

    flappy working but needs styling

--- Flappy-Bird/app.js
+++ Flappy-Bird/app.js
@@ -2,10 +2,11 @@ const bird = document.querySelector(".bird");
 const gameDisplay = document.querySelector(".game-container");
 const ground = document.querySelector(".ground");
 
-let birdLeft = 200;
+let birdLeft = 220;
 let birdBottom = 100;
 let gravity = 2;
 let isGameOver = false;
+let gap = 530;
 // move bird to bottom center of sky div
 function startGame() {
   birdBottom -= gravity;
@@ -30,41 +31,58 @@ function jump() {
   // each time the jump function is called adding 50px but not if it would cause bird to jump off page
   if (birdBottom < 500) birdBottom += 50;
   bird.style.bottom = birdBottom + "px";
-  console.log(birdBottom);
+  // console.log(birdBottom);
 }
 document.addEventListener("keyup", control);
 
 function generateObstacle() {
   let obstacleLeft = 500;
   let randomHeight = Math.random() * 60;
-  console.log(randomHeight);
+  // console.log(randomHeight);
   let obstacleBottom = randomHeight;
   const obstacle = document.createElement("div");
-  obstacle.classList.add("obstacle");
+  const topObstacle = document.createElement("div");
+
+  if (!isGameOver) {
+    obstacle.classList.add("obstacle");
+    topObstacle.classList.add("topObstacle");
+  }
   gameDisplay.appendChild(obstacle);
+
+  gameDisplay.appendChild(topObstacle);
   obstacle.style.left = obstacleLeft + "px";
+  topObstacle.style.left = obstacleLeft + "px";
   obstacle.style.bottom = obstacleBottom + "px";
+  topObstacle.style.bottom = obstacleBottom + "px";
+  topObstacle.style.bottom = obstacleBottom + gap + "px";
   //   moving the obstacle
   function moveObstacle() {
     obstacleLeft -= 2;
     obstacle.style.left = obstacleLeft + "px";
+    topObstacle.style.left = obstacleLeft + "px";
+
     if (obstacleLeft === -60) {
       clearTimeout(timerId);
       gameDisplay.removeChild(obstacle);
+      gameDisplay.removeChild(topObstacle);
     }
+    // bird colides with obstacle or bird hits ground
     if (
-      // bird colides with obstacle
-      (obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220) ||
-      // bird hit ground
+      (obstacleLeft > 200 &&
+        obstacleLeft < 280 &&
+        birdLeft === 220 &&
+        (birdBottom < obstacleBottom + 153 ||
+          birdBottom > obstacleBottom + gap - 200)) ||
       birdBottom === 0
     ) {
       gameOver();
+      clearInterval(timerId);
     }
   }
   // move obstacle every 20ms
   let timerId = setInterval(moveObstacle, 20);
   // genrate more obstacles by putting generateObstacle in setTimeout
-  setTimeout(generateObstacle, 3000);
+  if (!isGameOver) setTimeout(generateObstacle, 3000);
 }
 
 generateObstacle();
@@ -74,3 +92,4 @@ function gameOver() {
   isGameOver = true;
   document.removeEventListener("keyup", control);
 }
+// https://www.youtube.com/watch?v=8xPsg6yv7TU

5ae857fb3c65e366ebb59baa2f9e98753835d0bf
AR123456 <coloradoz99@gmail.com>
2021-11-18 05:54:01 -0700

    adding and playing sound

--- JS-Snake-With-Sound-Adam-Version/app.js
+++ JS-Snake-With-Sound-Adam-Version/app.js
@@ -22,18 +22,83 @@ let xVelocity = 0;
 let yVelocity = 0;
 let appleX = 5;
 let appleY = 5;
+let score = 0;
+// create audio constructor for sounds
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement/Audio
+const gulpSound = new Audio("gulp.mp3");
 
 //game loop runs continueusly updating screen could be done in one of 3 ways
 // requestAnimationFrame
 //setInterval x times per second
 // setTimeOut -  using this to change how often the screen updates
 function drawGame() {
-  clearScreen();
   changeSnakePosition();
+  let result = isGameOver();
+  if (result) {
+    // game is over
+    return;
+  }
+  clearScreen();
+
   checkAppleCollision();
   drawApple();
   drawSnake();
+  drawScore();
+  // increase the difficulty of the game based on score
   setTimeout(drawGame, 1000 / speed);
+}
+function isGameOver() {
+  let gameOver = false;
+  // if Velocity is 0 the game has not started yet
+  if (yVelocity === 0 && xVelocity === 0) {
+    return false;
+  }
+
+  //walls using if statement ,  maybe switch case would be better
+  // left wall is hit
+  if (headX < 0) {
+    gameOver = true;
+  }
+  // right wall
+  else if (headX === tileCount) {
+    gameOver = true;
+  }
+  // top wall hit
+  else if (headY < 0) {
+    gameOver = true;
+    // hit bottom wall  tile count is 20 for 20x20 grid
+  } else if (headY === tileCount) {
+    gameOver = true;
+  }
+  // colison with snake body
+  for (let i = 0; i < snakeParts.length; i++) {
+    let part = snakeParts[i];
+    // are any of the parts in the same spot as the head
+    if (part.x === headX && part.y === headY) {
+      // velocity check needed or else game is over immediatly
+      gameOver = true;
+      // break out for loop
+      break;
+    }
+  }
+
+  if (gameOver) {
+    ctx.fillStyle = "white";
+    ctx.font = "50px Veranda";
+    // var gradiant = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    // gradiant.addColorStop("0", "magenta");
+    // gradiant.addColorStop("0.5", "blue");
+    // gradiant.addColorStop("1.0", "red");
+    // fill with gradient
+    // ctx.fillStyle = gradiant;
+    ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
+  }
+  return gameOver;
+}
+function drawScore() {
+  ctx.fillStyle = "white";
+  ctx.font = "10px verdana";
+  ctx.fillText("score: " + score, canvas.width - 50, 10);
 }
 function clearScreen() {
   ctx.fillStyle = "black";
@@ -50,9 +115,12 @@ function drawSnake() {
   }
   // put the new part where the head was
   snakeParts.push(new SnakePart(headX, headY));
-  if (snakeParts.length > tailLength) {
-    // remove snake part
-    snakeParts.shift();
+  // if (snakeParts.length > tailLength) {
+  //   // remove snake part
+  //   snakeParts.shift();
+  // }
+  while (snakeParts.length > tailLength) {
+    snakeParts.shift(); // remove the furthes item from snake parts array
   }
 }
 function changeSnakePosition() {
@@ -72,6 +140,9 @@ function checkAppleCollision() {
     appleX = Math.floor(Math.random() * tileCount);
     appleY = Math.floor(Math.random() * tileCount);
     tailLength++;
+    score++;
+    // play the sound
+    gulpSound.play();
   }
 }
 

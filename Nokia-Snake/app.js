b7d059f1019a954effad92bfb5e8d7fa46999195
AR123456 <coloradoz99@gmail.com>
2021-11-20 08:22:43 -0700

    final game nokia snake

--- Nokia-Snake/app.js
+++ Nokia-Snake/app.js
@@ -7,7 +7,7 @@ let currentIndex = 0; //so first div in our grid
 let appleIndex = 0; //so first div in our grid
 // divs in grid begin at 2 is the  head and 0 being the end , 1 will be the body
 let currentSnake = [2, 1, 0];
-let direction = 1;
+let direction = 1; // snake should always go one div down the array
 let score = 0;
 let speed = 0.9;
 let intervalTime = 0;
@@ -23,27 +23,29 @@ function startGame() {
   direction = 1;
   scoreDisplay.innerText = score;
   intervalTime = 1000;
-  progresive;
   currentSnake = [2, 1, 0];
+  currentIndex = 0;
   currentSnake.forEach((index) => squares[index].classList.add("snake"));
-  interval = setInterval(moveOutcomes, intervalTime); // moveOutcomes will deal with collisons,wins and score setting
+  interval = setInterval(moveOutcomes, intervalTime);
 }
-//function that deals with move outcomes
+//function that deals with ALL the ove outcomes of the Snake
 function moveOutcomes() {
-  // snake hits border
+  //deals with snake hitting border and snake hitting self
   if (
-    (currentSnake[0] + width >= width * width && direction === width) ||
-    (currentSnake[0] % width === width - 1 && direction === 1) ||
-    (currentSnake[0] % width === 0 && direction === -1) ||
-    (currentSnake[0] - width < 0 && direction === -width) ||
-    squares[currentSnake[0] + direction].classList.contains("snake")
+    (currentSnake[0] + width >= width * width && direction === width) || //if snake hits bottom
+    (currentSnake[0] % width === width - 1 && direction === 1) || //if snake hits right wall
+    (currentSnake[0] % width === 0 && direction === -1) || //if snake hits left wall
+    (currentSnake[0] - width < 0 && direction === -width) || //if snake hits the top
+    squares[currentSnake[0] + direction].classList.contains("snake") //if snake goes into itself
   ) {
-    clearInterval(interval);
+    return clearInterval(interval); //this will clear the interval if any of the above happen
   }
-  const tail = currentSnake.pop();
-  squares[tail].classList.remove("snake");
-  currentSnake.unshift(currentSnake[0] + direction);
-  // dealing with the snake hitting the apple
+
+  const tail = currentSnake.pop(); //removes last ite of the array and shows it
+  squares[tail].classList.remove("snake"); //removes class of snake from the TAIL
+  currentSnake.unshift(currentSnake[0] + direction); //gives direction to the head of the array
+
+  //deals with snake getting apple
   if (squares[currentSnake[0]].classList.contains("apple")) {
     squares[currentSnake[0]].classList.remove("apple");
     squares[tail].classList.add("snake");
@@ -57,29 +59,27 @@ function moveOutcomes() {
   }
   squares[currentSnake[0]].classList.add("snake");
 }
-
-function control(e) {
-  squares[currentIndex].classList.remove("snake"); /// remove snake class from current index
-  if (e.keyCode === 39) {
-    direction = 1; // if right arrow key is pressed snake will go right one
-    console.log("right", currentSnake);
-  } else if (e.keyCode === 38) {
-    direction = -width; // if left arrow key is pressed snake will go left one
-    console.log("left", currentSnake);
-  } else if (e.keyCode === 37) {
-    direction = -1; // if up arrow key is pressed snake will go up one
-    console.log("up", currentSnake);
-  } else if (e.keyCode === 40) {
-    console.log("down", currentSnake);
-    direction = +width; // if down arrow key is pressed snake will go down one
-  }
-}
+//generate new apple once apple is eaten
 function randomApple() {
-  //   do {
-  //     appleIndex = Math.floor(Math.random() * squares.length);
-  //   } while (squares[appleIndex].classList.contains("snake"));
-  //   squares[appleIndex].classList.add("apple");
-  console.log("make a new apple");
+  do {
+    appleIndex = Math.floor(Math.random() * squares.length);
+  } while (squares[appleIndex].classList.contains("snake")); //making sure apples dont appear on the snake
+  squares[appleIndex].classList.add("apple");
+}
+
+//assign functions to keycodes
+function control(e) {
+  squares[currentIndex].classList.remove("snake");
+
+  if (e.keyCode === 39) {
+    direction = 1; //if we press the right arrow on our keyboard, the snake will go right one
+  } else if (e.keyCode === 38) {
+    direction = -width; // if we press the up arrow, the snake will go back ten divs, appearing to go up
+  } else if (e.keyCode === 37) {
+    direction = -1; // if we press left, the snake will go left one div
+  } else if (e.keyCode === 40) {
+    direction = +width; //if we press down, the snake head will instantly appear in the div ten divs from where you are now
+  }
 }
 document.addEventListener("keyup", control);
 startBtn.addEventListener("click", startGame);

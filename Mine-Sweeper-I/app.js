adb7e94f13d62485a2329730e5c3fc93ccf84727
AR123456 <coloradoz99@gmail.com>
2021-11-26 05:32:19 -0700

    create board

--- Mine-Sweeper-I/app.js
+++ Mine-Sweeper-I/app.js
@@ -1,6 +1,11 @@
 // for Display / UI 
 // 1 populate board with mines - logic and display parts
+import {createBoard} from "./game.js"
+
+console.log(createBoard(2,2))
 // 2 left click on tiles reviels 
+
+
 // 3 right click on tiles- marks
 // 4 check for win/ loss
 
--- Mine-Sweeper-I/game.js
+++ Mine-Sweeper-I/game.js
@@ -1,4 +1,25 @@
 // game logic 
-function createBoard(boardSize, numberOfMines){
-    //
+const TILE_STATUSES = {
+    HIDDEN:"hidden" ,
+    MINE:"mine", 
+    NUMBER:"number", 
+    MARKED:"marked"
+}
+export function createBoard(boardSize, numberOfMines){
+    //board is an array of arrays 
+    const board =[]
+    for (let x = 0; x < boardSize; x++) {
+        const row =[]
+        for (let y = 0; y < boardSize; y++) {
+        const element = document.createElement("div")    
+        const tile = {
+            x,
+            y,
+        }     
+        row.push(tile) 
+        }
+    board.push(row) 
+    }
+
+    return board
 }

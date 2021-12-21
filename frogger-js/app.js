5340e9eb409140912482bf2d213ece2d79207132
AR123456 <coloradoz99@gmail.com>
2021-12-21 06:52:47 -0700

    frogger switch case

--- frogger-js/app.js
+++ frogger-js/app.js
@@ -7,5 +7,32 @@ const startBtn = document.querySelector("#button");
 
 const width = 9;
 let currentIndex = 76;
+let timerId;
 
 squares[currentIndex].classList.add("frog");
+// switch cases for moving with keys
+function moveFrog(e) {
+  squares[currentIndex].classList.remove("frog");
+  switch (e.keyCode) {
+    // left 37
+    case 37:
+      if (currentIndex % width !== 0) currentIndex -= 1;
+      break;
+    // up 38
+    case 38:
+      if (currentIndex - width >= 0) currentIndex -= width;
+      break;
+    // right 39
+    case 39:
+      if (currentIndex % width < width - 1) currentIndex += 1;
+      break;
+    //down 40
+    case 40:
+      if (currentIndex + width < width * width) currentIndex += width;
+      break;
+  }
+  squares[currentIndex].classList.add("frog");
+  //   lose();
+  //   window();
+}
+document.addEventListener("keyup", moveFrog);

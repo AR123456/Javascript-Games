e109957b6133399516d5fba0b2a4b968ffb7c323
AR123456 <coloradoz99@gmail.com>
2021-12-22 06:48:34 -0700

    mindesweeper create game board

--- Mine-Sweeper-I/app.js
+++ Mine-Sweeper-I/app.js
@@ -1,29 +1,17 @@
-// for Display / UI 
-// 1 populate board with mines - logic and display parts
-import {createBoard} from "./game.js"
+const grid = document.querySelector(".grid");
 
-const BOARD_SIZE = 10;
-const NUMBER_OF_MINES = 2;
- 
-const board =(createBoard(BOARD_SIZE,NUMBER_OF_MINES))
-const boardElement = document.querySelector(".board")
+let width = 10;
+let height = 10;
+let squares = [];
 
-board.forEach(row =>{
-    row.forEach(tile =>{
-        boardElement.append(tile.element)
-    })
-})
-// set BOARD_SIZE to css --size property
-boardElement.style.setProperty("--size",BOARD_SIZE)
-
-
-
-// 2 left click on tiles reviels 
-
-
-// 3 right click on tiles- marks
-// 4 check for win/ loss
-
-// 2 files game logic and display logic 
-
-
+// create Board function
+function createBoard() {
+  for (let i = 0; i < width * height; i++) {
+    const square = document.createElement("div");
+    square.setAttribute("id", i);
+    grid.appendChild(square);
+    squares.push(square);
+  }
+}
+createBoard();
+console.log(squares);

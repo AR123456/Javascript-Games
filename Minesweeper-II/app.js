abd41381bb25dc62d71271f1d4d0483d54db0aac
AR123456 <coloradoz99@gmail.com>
2021-12-23 05:50:19 -0700

    non working random bomb placement

--- Minesweeper-II/app.js
+++ Minesweeper-II/app.js
@@ -8,14 +8,20 @@ let bombAmount = 20;
 
 // create Board function
 function createBoard() {
-  // create the bombs
+  let mixedArray = [];
   // make array of 20 indexes
   const bombsArray = Array(bombAmount).fill("bomb");
   // empty array of the remaining squares (100-20)
   const emptyArray = Array(width * height - bombAmount).fill("valid");
-  console.log(bombsArray);
-  console.log(emptyArray);
+  //   console.log(bombsArray);
+  //   console.log(emptyArray);
   // join the 2 arrays and mix them up randomly
+  // join them
+  mixedArray.push(...bombsArray, ...emptyArray);
+  //   console.log(mixedArray);
+
+  let mixedArrayRandom = shuffle(mixedArray);
+  //   console.log(mixedArrayRandom);
   for (let i = 0; i < width * height; i++) {
     const square = document.createElement("div");
     square.setAttribute("id", i);
@@ -24,3 +30,17 @@ function createBoard() {
   }
 }
 createBoard();
+function shuffle(mixedArray) {
+  let currentIndex = mixedArray.length,
+    mixedArrayRadomIndex;
+  while (currentIndex != 0) {
+    mixedArrayRadomIndex = Math.floor(Math.random() * currentIndex);
+    --currentIndex;
+    [mixedArray[currentIndex], mixedArray[mixedArrayRadomIndex]] = [
+      mixedArray[mixedArrayRadomIndex],
+      mixedArray[currentIndex],
+    ];
+    console.log(mixedArray);
+    return mixedArray;
+  }
+}

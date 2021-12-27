af4df22a856fa9a58c2d825433dc218e1dd4edb6
AR123456 <coloradoz99@gmail.com>
2021-12-27 05:10:27 -0700

    mine sweeper remobing extra comments

--- Minesweeper-II/app.js
+++ Minesweeper-II/app.js
@@ -13,22 +13,15 @@ function createBoard() {
   const bombsArray = Array(bombAmount).fill("bomb");
   // empty array of the remaining squares (100-20)
   const emptyArray = Array(width * height - bombAmount).fill("valid");
-  //   console.log(bombsArray);
-  //   console.log(emptyArray);
   // join the 2 arrays and mix them up randomly
-  // join them
+
   const gameArray = emptyArray.concat(bombsArray);
   // mix them
-  //   https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
   const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
-  // console.log(shuffledArray);
-  // another way using Fisher-Yates algorith
-  //https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
 
   for (let i = 0; i < width * height; i++) {
     const square = document.createElement("div");
     square.setAttribute("id", i);
-    // give the square a class name
     square.classList.add(shuffledArray[i]);
     grid.appendChild(square);
     squares.push(square);
@@ -38,7 +31,7 @@ function createBoard() {
       click(square);
     });
   }
-  // putting numbers into squares that are the bombs neighbors
+  // checking for bombs
   for (let i = 0; i < squares.length; i++) {
     let total = 0;
     // if square on right or left side of grid do not check it
@@ -48,59 +41,37 @@ function createBoard() {
 
     // checking each direction - if a square is not a bomb
     if (squares[i].classList.contains("valid")) {
-      // check square to the west  https://www.youtube.com/watch?v=W0No1JDc6vE
-      // if the square is not on the left edge && square to its east is a bomb increment total
-      if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains("bomb")) {
+      if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains("bomb"))
         total++;
-      }
-      // check square to the north east  https://www.youtube.com/watch?v=W0No1JDc6vE
-      // if the square at least in the 10 index && not on the far right edge &&
-      //square + 10 - width(10) has a bomb incrment total
       if (
         i > 9 &&
         !isRightEdge &&
         squares[i + 1 - width].classList.contains("bomb")
-      ) {
+      )
         total++;
-      }
-      // check north   https://www.youtube.com/watch?v=W0No1JDc6vE
-      // if the square is at least 11 && square index - 10 has bomb increment
-      if (i > 10 && squares[i - width].classList.contains("bomb")) {
-        total++;
-      }
-      //check north west   https://www.youtube.com/watch?v=W0No1JDc6vE
+      if (i > 10 && squares[i - width].classList.contains("bomb")) total++;
       if (
         i > 11 &&
         !isLeftEdge &&
         squares[i - 1 - width].classList.contains("bomb")
-      ) {
+      )
         total++;
-      }
-      // check east   https://www.youtube.com/watch?v=W0No1JDc6vE
-      if (i < 98 && !isRightEdge && squares[i + 1].classList.contains("bomb")) {
+      if (i < 98 && !isRightEdge && squares[i + 1].classList.contains("bomb"))
         total++;
-      }
-      // south west  https://www.youtube.com/watch?v=W0No1JDc6vE
       if (
         i < 90 &&
         !isLeftEdge &&
         squares[i - 1 + width].classList.contains("bomb")
-      ) {
+      )
         total++;
-      }
-      //south east  https://www.youtube.com/watch?v=W0No1JDc6vE
       if (
         i < 88 &&
         !isRightEdge &&
         squares[i + 1 + width].classList.contains("bomb")
-      ) {
+      )
         total++;
-      }
-      // south    https://www.youtube.com/watch?v=W0No1JDc6vE
       if (i < 89 && squares[i + width].classList.contains("bomb")) total++;
-      // this sets the data attribute with the incremented total
       squares[i].setAttribute("data", total);
-      // console.log(squares[i]);
     }
   }
 }

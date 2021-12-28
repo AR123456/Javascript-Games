69487ca744ab73bb45e8aae03e8126077d9f997e
AR123456 <coloradoz99@gmail.com>
2021-12-28 06:47:24 -0700

    recursion keep after push

--- Minesweeper-II/app.js
+++ Minesweeper-II/app.js
@@ -42,8 +42,10 @@ function createBoard() {
 
     // checking each direction - if a square is not a bomb
     if (squares[i].classList.contains("valid")) {
+      //north east
       if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains("bomb"))
         total++;
+      //norht
       if (
         i > 9 &&
         !isRightEdge &&
@@ -51,26 +53,31 @@ function createBoard() {
       )
         total++;
       if (i > 10 && squares[i - width].classList.contains("bomb")) total++;
+      //norhtwest
       if (
         i > 11 &&
         !isLeftEdge &&
         squares[i - 1 - width].classList.contains("bomb")
       )
         total++;
+      //east
       if (i < 98 && !isRightEdge && squares[i + 1].classList.contains("bomb"))
         total++;
+      //south west
       if (
         i < 90 &&
         !isLeftEdge &&
         squares[i - 1 + width].classList.contains("bomb")
       )
         total++;
+      //south east
       if (
         i < 88 &&
         !isRightEdge &&
         squares[i + 1 + width].classList.contains("bomb")
       )
         total++;
+      //souht
       if (i < 89 && squares[i + width].classList.contains("bomb")) total++;
       squares[i].setAttribute("data", total);
     }
@@ -111,4 +118,57 @@ function click(square) {
 function checkSquare(square, currentId) {
   //check the 8 surrounding squares of any square we are checking
   // and make sure we are not at an edge
+  const isLeftEdge = currentId % width === 0;
+  const isRightEdge = currentId % width === -1;
+  //put this in set time out so there is a pause before we go into the recursion
+  setTimeout(() => {
+    if (currentId > 0 && !isLeftEdge) {
+      //north east
+      const newId = squares[parseInt(currentId) - 1].id;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    if (currentId > 9 && !isRightEdge) {
+      //sw square
+      const newId = squares[parseInt(currentId) + 1 - width].id;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    if (currentId > 10) {
+      //north
+      const newId = squares[parseInt(currentId - width)].id;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    if (currentId > 11 && !isLeftEdge) {
+      //north west
+      const newId = squares[parseInt(currentId) - 1 - width].id;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    if (currentId < 98 && !isRightEdge) {
+      //east
+      const newId = squares[parseInt(currentId) + 1].id;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    if (currentId < 90 && !isLeftEdge) {
+      // south west
+      const newId = squares[parseInt(currentId) - 1 + width].id;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    if (currentId < 88 && !isRightEdge) {
+      // south east
+      const newId = squares[parseInt(currentId) + 1 + width].id;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    if (currentId < 89) {
+      // south
+      const newId = squares[parseInt(currentId) + width].id;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+  }, 10);
 }

0406032bc08f18b00f1c9db8723bdc85c1b340fd
AR123456 <coloradoz99@gmail.com>
2021-12-29 07:25:13 -0700

    mine sweeper mark and count reveals

--- Mine-Sweeper-I/app.js
+++ Mine-Sweeper-I/app.js
@@ -1,38 +1,47 @@
-// for Display / UI 
+// for Display / UI
 // 1 populate board with mines - logic and display parts
-import {createBoard,markTile} from "./game.js"
+import { createBoard, markTile, TILE_STATUSES, revealTile } from "./game.js";
 
 const BOARD_SIZE = 10;
-const NUMBER_OF_MINES = 2;
+const NUMBER_OF_MINES = 10;
 
- 
-const board =(createBoard(BOARD_SIZE,NUMBER_OF_MINES))
-const boardElement = document.querySelector(".board")
-const minesLeftText = document.querySelector("[data-mine-count]")
-board.forEach(row =>{
-    row.forEach(tile =>{
-        boardElement.append(tile.element)
-    })
-})
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
+const boardElement = document.querySelector(".board");
+const minesLeftText = document.querySelector("[data-mine-count]");
+board.forEach((row) => {
+  row.forEach((tile) => {
+    boardElement.append(tile.element);
+
+    // 2 left click on tiles reveals
+    tile.element.addEventListener("click", () => {
+      //
+    });
+
+    // 3 right click on tiles- marks-marks mean there is a mine there
+    // note cannot use click again as event use contextmenu which is what happens on a left click
+    tile.element.addEventListener("contextmenu", (e) => {
+      //the default behavior here is the context menue so prevent that
+      e.preventDefault();
+      markTile(tile);
+      // dislay the number of mines left
+      listMinesLeft();
+    });
+  });
+});
+
+function listMinesLeft() {
+  // count up the tiles that are marked and display them
+  const markedTilesCount = board.reduce((count, row) => {
+    return (
+      count + row.filter((tile) => tile.status === TILE_STATUSES.MARKED).length
+    );
+  }, 0);
+  minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount;
+}
 // set BOARD_SIZE to css --size property
-boardElement.style.setProperty("--size",BOARD_SIZE)
-minesLeftText.textContent = NUMBER_OF_MINES
+boardElement.style.setProperty("--size", BOARD_SIZE);
+minesLeftText.textContent = NUMBER_OF_MINES;
 
-
-// 2 left click on tiles reveals 
-tile.element.addEventListener("click",()=>{
-    // 
-})
-
-// 3 right click on tiles- marks
-// note cannot use click again as event use contextmenu which is what happens on a left click
-tile.element.addEventListener("contextmenu", e => {
-    //the default behavior here is the context menue so prevent that
-    e.preventDefault();
-    markTile(tile);
-})
 // 4 check for win/ loss
 
-// 2 files game logic and display logic 
-
-
+// 2 files game logic and display logic
--- Mine-Sweeper-I/game.js
+++ Mine-Sweeper-I/game.js
@@ -1,84 +1,82 @@
-// game logic 
+// game logic
 // https://
 // https://
-// 
-const TILE_STATUSES = {
-    HIDDEN:"hidden" ,
-    MINE:"mine", 
-    NUMBER:"number", 
-    MARKED:"marked"
-}
-export function createBoard(boardSize, numberOfMines){
-    //board is an array of arrays 
-    const board =[]
-    // to create mines- array of values 
-    const minePositions = getMinePositions(boardSize,numberOfMines)
-    console.log(minePositions)
-    for (let x = 0; x < boardSize; x++) {
-        const row =[]
-        for (let y = 0; y < boardSize; y++) {
-        
-        const element = document.createElement("div")    
-        element.dataset.status = TILE_STATUSES.HIDDEN
-        const tile = {
-            element,
-            x,
-            y,
-            // number of mines 
-            mine:minePositions.some(positionMatch.bind(null,{x,y})),
-            //getter setter  change status based on click
-            get status(){
-                return this.element.dataset.status
-            },
-            set status(value){
-                this.element.dataset.status=value
-            }
-        }     
-        row.push(tile) 
-        }
-    board.push(row) 
+//
+export const TILE_STATUSES = {
+  HIDDEN: "hidden",
+  MINE: "mine",
+  NUMBER: "number",
+  MARKED: "marked",
+};
+export function createBoard(boardSize, numberOfMines) {
+  //board is an array of arrays
+  const board = [];
+  // to create mines- array of values
+  const minePositions = getMinePositions(boardSize, numberOfMines);
+  console.log(minePositions);
+  for (let x = 0; x < boardSize; x++) {
+    const row = [];
+    for (let y = 0; y < boardSize; y++) {
+      const element = document.createElement("div");
+      element.dataset.status = TILE_STATUSES.HIDDEN;
+      const tile = {
+        element,
+        x,
+        y,
+        // number of mines
+        mine: minePositions.some(positionMatch.bind(null, { x, y })),
+        //getter setter  change status based on click
+        get status() {
+          return this.element.dataset.status;
+        },
+        set status(value) {
+          this.element.dataset.status = value;
+        },
+      };
+      row.push(tile);
     }
-console.log(board)
-    return board
-    
+    board.push(row);
+  }
+  console.log(board);
+  return board;
 }
 
-export function markTile(tile){
-    // is the tile seletceted eligible ? Only if it is 
-    //hidden or un mark a tile that is already marked 
-    if(tile.status!== TILE_STATUSES.HIDDEN &&
-        tile.status !== TILE_STATUSES.MARKED){
-            return
-        }
-    if(tile.status === TILE_STATUSES.MARKED){
-        tile.status = TILE_STATUSES.HIDDEN
-    }else{
-        tile.status = TILE_STATUSES.MARKED
-    }
+export function markTile(tile) {
+  if (
+    tile.status !== TILE_STATUSES.HIDDEN &&
+    tile.status !== TILE_STATUSES.MARKED
+  ) {
+    return;
+  }
+  // are we marking our un marking the tile
+  if (tile.status === TILE_STATUSES.MARKED) {
+    tile.status = TILE_STATUSES.HIDDEN;
+  } else {
+    tile.status = TILE_STATUSES.MARKED;
+  }
 }
-function getMinePositions(boardSize, numberOfMines){
-    const positions =[]
-   while (positions.length < numberOfMines){
-       const position = {
-           x:randomNumber(boardSize),
-           y:randomNumber(boardSize)
-       }
+function getMinePositions(boardSize, numberOfMines) {
+  const positions = [];
+  while (positions.length < numberOfMines) {
+    const position = {
+      x: randomNumber(boardSize),
+      y: randomNumber(boardSize),
+    };
     //    if any of the postions in the position array match positionMatch are not a match
 
     //    if(!positions.some(p => positionMatch(p.position)))
-    // witten using bind looks like this 
-       if(!positions.some(positionMatch.bind(null,position)))
-       {
-           // add position to the array
-           positions.push(position)
-       }
-   }
+    // witten using bind looks like this
+    if (!positions.some(positionMatch.bind(null, position))) {
+      // add position to the array
+      positions.push(position);
+    }
+  }
 
-    return  positions
+  return positions;
 }
-function positionMatch(a,b){
-    return a.x ===b.x && a.y === b.y
+function positionMatch(a, b) {
+  return a.x === b.x && a.y === b.y;
 }
-function randomNumber(size){
-return Math.floor(Math.random()*size)
+function randomNumber(size) {
+  return Math.floor(Math.random() * size);
 }

ff6458fe45a168a5084308c804ef85ca9cd6df77
AR123456 <coloradoz99@gmail.com>
2022-01-03 06:39:27 -0700

    space invaders move shooter switch case

--- space-invaders-Ania/app.js
+++ space-invaders-Ania/app.js
@@ -2,7 +2,7 @@ const squares = document.querySelectorAll(".grid div");
 const resultDisplay = document.getElementById("result");
 let width = 15;
 let currentShooterIndex = 202;
-let currentINvaderIndex = 0;
+let currentInvaderIndex = 0;
 let alienInvadersTakenDown = [];
 let result = 0;
 let direction = 1;
@@ -15,5 +15,28 @@ const alienInvaders = [
 ];
 // draw alien inaders - add the class list invader to these squares
 alienInvaders.forEach((invader) =>
-  squares[currentINvaderIndex + invader].classList.add("invader")
+  squares[currentInvaderIndex + invader].classList.add("invader")
 );
+// shooter
+
+squares[currentShooterIndex].classList.add("shooter");
+// move shooter along the bottom row
+// use key codes for the eventlistener  37 abd 39 left and right arrows
+function moveShooter(e) {
+  squares[currentInvaderIndex].classList.remove("shooter");
+  switch (e.keyCode) {
+    case 37:
+      // 37 is move left check to see if next to l edge
+      // if the index / width is not even number it is ok to move
+      if (currentShooterIndex % width !== 0) currentShooterIndex -= 1;
+
+      break;
+    case 39:
+      // 39 is move right  check to see if far right edge
+      // if index divisable by the width(15) and the number is less that 14 it is ok to move right
+      if (currentShooterIndex % width < width - 1) currentShooterIndex += 1;
+      break;
+  }
+  squares[currentShooterIndex].classList.add("shooter");
+}
+// e

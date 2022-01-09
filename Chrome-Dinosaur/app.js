045bcf4d5b3893bf9c43279b9fe774b861f1a655
AR123456 <coloradoz99@gmail.com>
2022-01-08 18:17:16 -0700

    dinos score

--- Chrome-Dinosaur/app.js
+++ Chrome-Dinosaur/app.js
@@ -1,7 +1,9 @@
 import { updateGround, setupGround } from "./ground.js";
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
+const SPEED_SCALE_INCREASE = 0.00001;
 const worldElem = document.querySelector("[data-world]");
+const scoreElem = document.querySelector("[data-score]");
 
 setPixelToWorldScale();
 
@@ -10,6 +12,7 @@ document.addEventListener("keydown", handleStart, { once: true });
 
 let lastTime;
 let speedScale;
+let score;
 function update(time) {
   if (lastTime === null) {
     lastTime = time;
@@ -24,12 +27,18 @@ function update(time) {
 
   window.requestAnimationFrame(update);
 }
-function updateSpeedScale() {
-  //
+function updateSpeedScale(delta) {
+  speedScale += delta * SPEED_SCALE_INCREASE;
+}
+
+function updateScore(delta) {
+  score += delta * 0.01;
+  scoreElem.textContent = Math.floor(score);
 }
 function handleStart() {
   lastTime = null;
   speedScale = 1;
+  score = 0;
   setupGround();
   window.requestAnimationFrame(update);
 }

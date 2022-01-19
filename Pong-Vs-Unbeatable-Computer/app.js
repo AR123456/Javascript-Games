c1017e2e6b07fb3c0af615aa4d7a35b579271af1
AR123456 <coloradoz99@gmail.com>
2022-01-19 05:14:36 -0700

    use delta

--- Pong-Vs-Unbeatable-Computer/app.js
+++ Pong-Vs-Unbeatable-Computer/app.js
@@ -2,17 +2,26 @@
 import Ball from "./Ball.js";
 // use and interact with ball.js
 const ball = new Ball(document.getElementById("ball"));
+let lastTime;
 
 function update(time) {
   // how much time has passed since time program started
   // with every frame the update function will be called
-  console.log(time);
+  // the time is incrementing with each render, sot take the time and convert it
+  // to a delta   delta = amount of time passed from prior frame to current frame
+  // https://www.youtube.com/watch?v=PeY6lXPrPaA
+  // the very first time we run this the lastTime will be null so need to check for that, hense the if statment
+  if (lastTime != null) {
+    // do all the important code we have a delta sinnce lastTime is not null so good to go
+    const delta = time - lastTime;
+    // Update code stuff here
+    console.log(delta);
+  }
+
+  //   const delta = time - lastTime;
+  lastTime = time;
+
   window.requestAnimationFrame(update);
 }
-// putting a set time out here instead of this is not as good.
-// IE setINterval(update,10)
-//setInterval is not supper accrate, it may not always run every 10ms so problmes related to that.   Will also run inbetween frames, with requestAnimationFrame what happens is every time that you can change what is on the screen this funciton is going to run.  JS is smart enought to say hey you cannot change anything on screen so do not even bother running code.  So as soon as you can run something on the screen JS calls the function for us.
-// as long as we keep calling window.requestAnimaitonFrame it is going to infinitely loop.
-// will be called every time something on ours screen is allowed to change
+
 window.requestAnimationFrame(update);
-// https://www.youtube.com/watch?v=PeY6lXPrPaA

30d22deb8a050cf5f201db563530a64c8771e1bb
AR123456 <coloradoz99@gmail.com>
2022-01-19 05:15:13 -0700

    set dir with cos and sin

--- Pong-Vs-Unbeatable-Computer/Ball.js
+++ Pong-Vs-Unbeatable-Computer/Ball.js
@@ -1,5 +1,59 @@
 export default class Ball {
   constructor(ballElem) {
-    this.ballElem;
+    this.ballElem = ballElem;
+    this.reset();
   }
+
+  // helper functions to set x and y
+  //getter
+  get x() {
+    // x is defined in CSS as a CSS variable
+    // turn css into a javascript usable upnber
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"));
+  }
+  // setter
+  set x(value) {
+    //
+    this.ballElem.style.setProperty("--x", value);
+  }
+
+  get y() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"));
+  }
+  set y(value) {
+    this.ballElem.style.setProperty("--y", value);
+  }
+  // reset function will allow us to set x,y velocity and direction  when ball is created in the constructor
+  reset() {
+    this.x = 50;
+    this.y = 50;
+    // the length is always one in this calculation unit vector
+    // https://www.youtube.com/watch?v=PeY6lXPrPaA
+    // unit vector movment in direction of a
+    // vector allows for direction and speed to differ and not increase oneanother
+    this.direction = { x: 0 };
+    // the while loop is looking at x or horizontal movement if not very far do what is in the while loop which will introduce move movement in x and y directtions
+    while (
+      Math.abs(this.direction.x <= 0.2) ||
+      Math.abs(this.direction.x >= 0.9)
+    ) {
+      // get the heading 0 -360 deg as a readiant - so cosin and sin can be used to determine the x and y
+      const heading = randomNumberBetween(0, 2 * Math.PI);
+      // unit vector for the directions
+      this.direction = { x: Math.cos(heading), y: Math.sin(heading) };
+    }
+    // this console log is an object
+    console.log(this.direction);
+  }
+
+  update(delta) {
+    // update the x and y position of the ball - in % away from top left corner
+    // update the velocity and direction of movement
+    this.x = 5;
+    this.y = 15;
+  }
+}
+
+function randomNumberBetween(min, max) {
+  return Math.random() * (max - min) + min;
 }
--- Pong-Vs-Unbeatable-Computer/app.js
+++ Pong-Vs-Unbeatable-Computer/app.js
@@ -5,20 +5,15 @@ const ball = new Ball(document.getElementById("ball"));
 let lastTime;
 
 function update(time) {
-  // how much time has passed since time program started
-  // with every frame the update function will be called
-  // the time is incrementing with each render, sot take the time and convert it
-  // to a delta   delta = amount of time passed from prior frame to current frame
-  // https://www.youtube.com/watch?v=PeY6lXPrPaA
-  // the very first time we run this the lastTime will be null so need to check for that, hense the if statment
   if (lastTime != null) {
     // do all the important code we have a delta sinnce lastTime is not null so good to go
     const delta = time - lastTime;
     // Update code stuff here
-    console.log(delta);
+    // console.log(delta);
+    // pass in the delta so that game operations are based off of it.
+    ball.update(delta);
   }
 
-  //   const delta = time - lastTime;
   lastTime = time;
 
   window.requestAnimationFrame(update);

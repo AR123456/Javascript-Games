5046f60f7ae289ce756b0d45d0c2186ca69f4516
AR123456 <coloradoz99@gmail.com>
2022-01-22 07:41:49 -0700

    flappy set up bird functions

---
+++ Flappy-Bird-Kyle/bird.js
@@ -0,0 +1,23 @@
+const birdEle = document.querySelector("[data-bird]");
+const BIRD_SPEED = 0.5;
+const JUMP_DURATION = 125;
+let timeSinceLastJump = Number.POSITIVE_INFINITY;
+
+export function setupBird() {
+  //
+}
+export function updateBird() {
+  //
+}
+export function getBirdRect() {
+  //
+}
+function setTop() {
+  //
+}
+function getTop() {
+  //
+}
+function handleJump(e) {
+  //
+}

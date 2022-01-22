a909c80c786ab77123ef3681c90a4ee556c7fb3c
AR123456 <coloradoz99@gmail.com>
2022-01-22 08:16:27 -0700

    set up app functions

---
+++ Flappy-Bird-Kyle/app.js
@@ -0,0 +1,26 @@
+import { updateBird, setupBird, getBirdRect } from "./bird.js";
+import {
+  updatePipes,
+  setupPipes,
+  getPassedPipesCount,
+  getPipeRects,
+} from "./pipe.js";
+
+const title = document.querySelector("[data-title]");
+const subtitle = document.querySelector("[data-subtitle]");
+
+function updateLoop() {
+  //
+}
+function checkLose() {
+  //
+}
+function isCollision() {
+  //
+}
+function handleStart() {
+  //
+}
+function handleLose() {
+  //
+}

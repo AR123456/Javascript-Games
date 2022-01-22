67d03670be71ada6789650d38a574d830aed7a6e
AR123456 <coloradoz99@gmail.com>
2022-01-22 08:16:02 -0700

    flappy set up pipe

---
+++ Flappy-Bird-Kyle/pipe.js
@@ -0,0 +1,25 @@
+const HOLE_HIGHT = 30;
+const PIPE_WIDTH = 120;
+const PIPE_INTERVAL = 1500;
+const PIPE_SPEED = 0.75;
+let pipes = [];
+let timeSinceLastPipe, passedPipeCount;
+
+export function setupPipes() {
+  //
+}
+export function updatePipes() {
+  //
+}
+export function getPassedPipesCount() {
+  //
+}
+function createPipe() {
+  //
+}
+function createPipeSegment() {
+  //
+}
+function randomNumberBetween() {
+  return Math.floor(Math.random() * (min - max + 1) + min);
+}

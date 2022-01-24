c3d959dc6e7d3a666c14d8b93367a3e1c65bfc60
AR123456 <coloradoz99@gmail.com>
2022-01-24 07:34:58 -0700

    flappy getter and setter

--- Flappy-Bird-Kyle/bird.js
+++ Flappy-Bird-Kyle/bird.js
@@ -1,22 +1,30 @@
-const birdEle = document.querySelector("[data-bird]");
+const birdElem = document.querySelector("[data-bird]");
 const BIRD_SPEED = 0.5;
 const JUMP_DURATION = 125;
 let timeSinceLastJump = Number.POSITIVE_INFINITY;
 
 export function setupBird() {
-  //
+  //this is the start function setting bird place on screen at game start
+  // put the bird in the middle of the screen
+  setTop(window.innerHeight / 2);
 }
-export function updateBird() {
-  //
+export function updateBird(delta) {
+  // bird falls by default, pass in the speed and use delta to account for frame changes
+
+  setTop(getTop() + BIRD_SPEED * delta);
+  console.log(getTop());
 }
 export function getBirdRect() {
   //
 }
-function setTop() {
-  //
+function setTop(top) {
+  // take in the top values to set the bird top in the css
+  birdElem.style.setProperty("--bird-top", top);
 }
 function getTop() {
-  //
+  // the birdEle property is actually computed so need to use this syntax to get it
+  // need to make sure this returns a number
+  return parseFloat(getComputedStyle(birdElem).getPropertyValue("--bird-top"));
 }
 function handleJump(e) {
   //

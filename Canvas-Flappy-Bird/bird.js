850f7ee1ffe400b76f5b660a0c11d0ca15510e56
AR123456 <coloradoz99@gmail.com>
2022-05-25 06:52:15 -0600

    broken dragon sprite for flappy bird

--- Canvas-Flappy-Bird/bird.js
+++ Canvas-Flappy-Bird/bird.js
@@ -1,10 +1,20 @@
+const dragonSprite = new Image();
+// the dragon png sprite sheet
+dragonSprite.src = "./dragon.png";
+
 class Bird {
   constructor() {
     this.x = 150;
     this.y = 200;
     this.vy = 0;
-    this.width = 20;
-    this.height = 20;
+    // one frame of the spritesheet
+    this.originaWidth = 941;
+    this.originaHeight = 680;
+    // this.width = 20;
+    // this.height = 20;
+    // adjust height and width to match that of the dragon.png the sprite sheet
+    this.width = this.originaWidth / 20;
+    this.height = this.originaHeight / 20;
     this.weight = 1;
   }
   update() {
@@ -27,7 +37,21 @@ class Bird {
   }
   draw() {
     ctx.fillStyle = "red";
+    // the hit box
     ctx.fillRect(this.x, this.y, this.width, this.height);
+    // pass in the required 9 items into this canvas function the long version
+    // syntax   ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+    ctx.drawImage(
+      dragonSprite,
+      0,
+      0,
+      this.originalWidth,
+      this.originalHeight,
+      this.x - 20,
+      this.y - 12,
+      this.width * 1.7,
+      this.height * 1.7
+    );
   }
   flap() {
     this.vy -= 2;

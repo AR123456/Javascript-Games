6928ed0362310e2be9cd75ffbc951b38362be2ce
AR123456 <coloradoz99@gmail.com>
2023-03-11 08:34:21 -0700

    setting up canvas alien planet

---
+++ Franks-Alien-Planet/app.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function () {
+  // when building games make sure the whole page has been loaded including style sheet and images
+  // wait until the page loads to setup the canvas
+  // set up canvas
+  const canvas = this.document.getElementById("canvas1");
+  // the drawing context object contains all methods and properties to draw and animate on HTML canvas
+  const ctx = canvas.getContext("2d");
+  canvas.width = 1500;
+  canvas.height = 500;
+});

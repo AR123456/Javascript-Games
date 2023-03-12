ecde606b196a1e718eb90dc5a9cac66e79e883c0
AR123456 <coloradoz99@gmail.com>
2023-03-12 09:04:21 -0600

    player constructor

--- Franks-Alien-Planet/app.js
+++ Franks-Alien-Planet/app.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function () {
   const canvas = this.document.getElementById("canvas1");
   // the drawing context object contains all methods and properties to draw and animate on HTML canvas
   const ctx = canvas.getContext("2d");
-  canvas.width = 1500;
+  canvas.width = 500;
   canvas.height = 500;
   // specified user inputs
   class InputHandler {}
@@ -14,8 +14,20 @@ window.addEventListener("load", function () {
   // falling scresn nuts and bolts
   class Particle {}
   // main character sprite sheet animation
-  class Player {}
-  // enemy tyoes
+  class Player {
+    // player needs game dimensions
+    constructor(game) {
+      this.game = game;
+      this.width = 120;
+      this.height = 150;
+      this.x=20;
+      this.y=100;
+    }
+    update(){
+    }
+    draw(){}
+  }
+  // enemy types
   class Enemy {}
   // inividual background layers
   class Layer {}

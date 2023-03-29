c57e6e8073cdab8225f6bce8dbc323b396195143
AR123456 <coloradoz99@gmail.com>
2023-03-29 01:35:15 -0600

    decease ammo

--- Franks-Alien-Planet/app.js
+++ Franks-Alien-Planet/app.js
@@ -22,7 +22,6 @@ window.addEventListener("load", function () {
         } else if (e.key === " ") {
           this.game.player.shootTop();
         }
-        console.log(this.game.keys);
       });
       // event listener to remove on keyup
       window.addEventListener("keyup", (e) => {
@@ -31,7 +30,6 @@ window.addEventListener("load", function () {
           // splice takes in the index of the key we want to remove , and the delete count
           this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
         }
-        console.log(this.game.keys);
       });
     }
   }
@@ -106,9 +104,14 @@ window.addEventListener("load", function () {
     }
     // custom methods on Player class for attack modes
     shootTop() {
-      // shoot projectiles from mouth of seahorse - pass in players position
-      this.projectiles.push(new Projectile(this.game, this.x, this.y));
-      console.log(this.projectiles);
+      // put some logic in place to dwindle shooting power that can be slowly replenished
+      if (this.game.ammo > 0) {
+        // shoot projectiles from mouth of seahorse - pass in players position
+        this.projectiles.push(new Projectile(this.game, this.x, this.y));
+        this.game.ammo--;
+
+        console.log(this.projectiles);
+      }
     }
   }
   // enemy types
@@ -131,6 +134,7 @@ window.addEventListener("load", function () {
       this.input = new InputHandler(this);
       // array to keep track of key presses- it is available to whole game
       this.keys = [];
+      this.ammo = 20;
     }
     update() {
       // this is the Player()'s update

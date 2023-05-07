e4366ca4ddc93b2318fbd9648486053d1a52b7fc
AR123456 <coloradoz99@gmail.com>
2023-05-07 08:25:37 -0600

    Physics with AI  loging game object

--- Animated-Physics-with-AI/app.js
+++ Animated-Physics-with-AI/app.js
@@ -7,13 +7,21 @@ window.addEventListener("load", function () {
   canvas.height = 720;
   //OOP make it modular
   class Player {
-    constructor() {}
+    constructor(game) {
+      this.game = game;
+    }
   }
   class Game {
     constructor(canvas) {
       this.canvas = canvas;
       this.width = this.canvas.width;
+      this.height = this.canvas.height;
+      // this is the Game object
+      this.player = new Player(this);
     }
   }
+  // create instance of game object
+  const game = new Game(canvas);
+  console.log(game);
   function animate() {}
 });

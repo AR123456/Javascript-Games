4c5bd1dee18acf9f67940cf3a1c281a97decb9f9
Anne <coloradoz99@gmail.com>
2023-05-21 05:07:28 -0600

    collision detection

--- Animated-Physics-with-AI/app.js
+++ Animated-Physics-with-AI/app.js
@@ -17,67 +17,9 @@ window.addEventListener("load", function () {
       this.collisionX = this.game.width * 0.5;
       this.collisionY = this.game.height * 0.5;
       // size of hit box
-      this.collisionRadius = 50;
-      this.speedX = 0;
-      this.speedY = 0;
-      // distances between mouse and player
-      this.dx = 0;
-      this.dy = 0;
-      // modify the speed of player
-      this.speedModifier = 5;
+      this.collisionRadius = 30;
     }
     // draw method
-    draw(context) {
-      // draw a circle
-      context.beginPath();
-      context.arc(
-        this.collisionX,
-        this.collisionY,
-        this.collisionRadius,
-        0,
-        Math.PI * 2
-      );
-      context.save();
-      context.globalAlpha = 0.5;
-      context.fill();
-      context.restore();
-      context.stroke();
-      // draw line off player to show direction of movement
-      context.beginPath();
-      context.moveTo(this.collisionX, this.collisionY);
-      context.lineTo(this.game.mouse.x, this.game.mouse.y);
-      context.stroke();
-    }
-    //  update method, make player move- call this in render on game
-    update() {
-      // assign dx dy
-      this.dx = this.game.mouse.x - this.collisionX;
-      this.dy = this.game.mouse.y - this.collisionY;
-      // calculate speed of x and y
-      // Math.hypot() expects y first then x
-      const distance = Math.hypot(this.dy, this.dx);
-      // only if distance is more
-      if (distance > this.speedModifier) {
-        this.speedX = this.dx / distance || 0;
-        this.speedY = this.dy / distance || 0;
-      } else {
-        this.speedX = 0;
-        this.speedY = 0;
-      }
-
-      this.collisionX += this.speedX * this.speedModifier;
-      this.collisionY += this.speedY * this.speedModifier;
-    }
-  }
-  // blueprint for individual obstacle objects
-  class Obstacle {
-    constructor(game) {
-      this.game = game;
-      // randomize distance to center of collision area circle
-      this.collisionX = Math.random() * this.game.width;
-      this.collisionY = Math.random() * this.game.height;
-      this.collisionRadius = 60;
-    }
     draw(context) {
       // draw a circle
       context.beginPath();
@@ -102,8 +44,6 @@ window.addEventListener("load", function () {
       this.height = this.canvas.height;
       // this is the Game object
       this.player = new Player(this);
-      // array to hold obstacles created
-      this.obstacles = [];
       this.mouse = {
         x: this.width * 0.5,
         y: this.height * 0.5,
@@ -124,27 +64,19 @@ window.addEventListener("load", function () {
         this.mouse.pressed = false;
       });
       canvas.addEventListener("mousemove", (e) => {
-        // only move player when mouse is pressed
-        if (this.mouse.pressed) {
-          //move
-          this.mouse.x = e.offsetX;
-          this.mouse.y = e.offsetY;
-        }
+        // get coordinates of click to use on game object event offset- so avalable to all of codebase
+        this.mouse.x = e.offsetX;
+        this.mouse.y = e.offsetY;
+        this.mouse.pressed = false;
       });
     }
     render(context) {
       this.player.draw(context);
-      this.player.update();
     }
   }
   // create instance of game object
   const game = new Game(canvas);
+  game.render(ctx);
 
-  function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // need to draw over and over to see so calling render from inside animation loop
-    game.render(ctx);
-    requestAnimationFrame(animate);
-  }
-  animate();
+  function animate() {}
 });

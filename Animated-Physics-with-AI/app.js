63acb6197b42172db5a2e6b15b6128a38860213b
AR123456 <coloradoz99@gmail.com>
2023-08-29 06:49:04 -0600

    bring in toad

--- Animated-Physics-with-AI/app.js
+++ Animated-Physics-with-AI/app.js
@@ -264,6 +264,7 @@ window.addEventListener("load", function () {
       this.collisionRadius = 30;
       this.collisionX = this.game.width;
       this.collisionY = Math.random() * this.game.height;
+      this.speedX = Math.random() * 3 + 0.5;
     }
     draw() {}
     update() {}

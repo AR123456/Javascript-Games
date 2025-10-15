bff3bc9bf6902e6bf2bb2c030c1b6a37579a286e
AR123456 <coloradoz99@gmail.com>
2025-10-15 06:16:22 -0400

    sketch out js file

--- JS-Gorilla-Game/app.js
+++ JS-Gorilla-Game/app.js
@@ -3,6 +3,10 @@ let state = {};
 
 // main canvas element and its drawing context
 const canvas = document.getElementById("game");
+canvas.width = window.innerWidth;
+canvas.height = window.height;
+const ctx = canvas.getContext("2d");
+console.log(ctx);
 // new game
 
 //main game function

5d8852f6393c66f6ddaecba5bb876b6a65abe5b1
AR123456 <coloradoz99@gmail.com>
2025-10-15 06:17:14 -0400

    buildings array defined

--- JS-Gorilla-Game/app.js
+++ JS-Gorilla-Game/app.js
@@ -1,10 +1,11 @@
-// State at start of game
+// State(game state) at start of game
 let state = {};
 
 // main canvas element and its drawing context
 const canvas = document.getElementById("game");
 canvas.width = window.innerWidth;
 canvas.height = window.height;
+// draw function needs context
 const ctx = canvas.getContext("2d");
 console.log(ctx);
 // new game
@@ -12,12 +13,20 @@ newGame();
 //main game function
 function newGame() {
   // reset game state
-  state = {};
-  //call draw function
+  state = {
+    // phase - aiming, in flight celebrating
+    //buildings  - array of positions and sizes
+    backgroundBuildings: [],
+  };
+  for (let i = 0; i < 11; i++) {
+    generateBackgroundBuilding(i);
+  }
+  //call draw function - paints the screen when called
   draw();
 }
 // draw function
 function draw() {}
 // event handlers
 function throwBomb() {}
+// calculate position of banana as it moves across the sky
 function animate(timestamp) {}

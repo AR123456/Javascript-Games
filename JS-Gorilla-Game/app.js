a63c9f127a2744efc209b628afc598b3e4571dd7
AR123456 <coloradoz99@gmail.com>
2025-10-16 06:46:14 -0400

    notes

--- JS-Gorilla-Game/app.js
+++ JS-Gorilla-Game/app.js
@@ -13,14 +13,7 @@ newGame();
 //main game function
 function newGame() {
   // reset game state
-  state = {
-    // phase - aiming, in flight celebrating
-    //buildings  - array of positions and sizes
-    backgroundBuildings: [],
-  };
-  for (let i = 0; i < 11; i++) {
-    generateBackgroundBuilding(i);
-  }
+  state = {};
   //call draw function - paints the screen when called
   draw();
 }

0058b8e57c79054a95ecc2283bf745ede5723516
AR123456 <coloradoz99@gmail.com>
2025-10-22 05:57:23 -0400

    now height

--- JS-Gorilla-Game/app.js
+++ JS-Gorilla-Game/app.js
@@ -53,6 +53,9 @@ function generateBackgroundBuilding(index) {
   const minWidth = 60;
   const maxWidth = 110;
   const width = minWidth + Math.random() * (maxWidth - minWidth);
+  const minHeight = 80;
+  const maxHeight = 350;
+  const height = minHeight + Math.random() * (maxHeight - minHeight);
 }
 function generateBuilding() {}
 function initializeBombPosition() {}

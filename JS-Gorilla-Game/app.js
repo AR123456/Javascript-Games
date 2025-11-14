53aeffdd875cf080aea2a7a5ef484e8108835cce
AR123456 <coloradoz99@gmail.com>
2025-11-14 06:20:32 -0500

    passing params to helper function

--- JS-Gorilla-Game/app.js
+++ JS-Gorilla-Game/app.js
@@ -164,9 +164,6 @@ function drawBomb() {
   // translate the coord system, move origin to center of bomb position S
   ctx.save();
   ctx.translate(state.bomb.x, state.bomb.y);
-  if (state.phase === "aiming") {
-    ctx.translate(-state.bomb.velocity.x / 6.25, -state.bomb.velocity.y / 6.25);
-  }
   // draw circle
   ctx.fillStyle = "white";
   ctx.beginPath();
@@ -176,9 +173,7 @@ function drawBomb() {
   ctx.restore();
 }
 // event handlers
-function throwBomb() {
-  console.log("throwing bomb");
-}
+function throwBomb() {}
 // calculate position of banana as it moves across the sky
 function animate(timestamp) {}
 function drawBackground() {
@@ -280,12 +275,7 @@ function drawGorillaLeftArm(player) {
   // check to see if the gorilla is aiming- gorilla on l is aiming by default
   if (state.phase === "aiming" && state.currentPlayer === 1 && player === 1) {
     // left hand goes up
-    ctx.quadraticCurveTo(
-      -44,
-      63,
-      -28 - state.bomb.velocity.x / 6.25,
-      107 - state.bomb.velocity.y / 6.25
-    );
+    ctx.quadraticCurveTo(-44, 63, -28, 107);
   } else if (state.phase === "celebrating" && state.currentPlayer === player) {
     ctx.quadraticCurveTo(-44, 63, -28, 107);
   } else {
@@ -301,12 +291,7 @@ function drawGorillaRightArm(player) {
   ctx.moveTo(+14, 50);
   // check to see if the gorilla is aiming- gorilla on r is aiming by default
   if (state.phase === "aiming" && state.currentPlayer === 2 && player === 2) {
-    ctx.quadraticCurveTo(
-      +44,
-      63,
-      +28 - state.bomb.velocity.x / 6.25,
-      107 - state.bomb.velocity.y / 6.25
-    );
+    ctx.quadraticCurveTo(+44, 63, +28, 107);
   } else if (state.phase === "celebrating" && state.currentPlayer === player) {
     ctx.quadraticCurveTo(+44, 63, +28, 107);
   } else {
@@ -371,16 +356,7 @@ function drawGorilla(player) {
 function setInfo(deltaX, deltaY) {
   // the trig to calc velocity ect
   const hypotenuse = Math.sqrt(deltaX ** 2 + deltaY ** 2);
-  // convert to Radians
-  const angleInRadians = Math.asin(deltaY / hypotenuse);
-  const angleInDegrees = (angleInRadians / Math.PI) * 160;
-  if (state.currentPlayer === 1) {
-    angle1DOM.innerText = Math.round(angleInDegrees);
-    velocity1DOM.innerText = Math.round(hypotenuse);
-  } else {
-    angle2DOM.innerText = Math.round(angleInDegrees);
-    velocity2DOM.innerText = Math.round(hypotenuse);
-  }
+  console.log(hypotenuse);
 }
 // event handler
 bombGrabAreaDOM.addEventListener("mousedown", function (e) {
@@ -401,18 +377,12 @@ window.addEventListener("mousemove", function (e) {
     state.bomb.velocity.x = -deltaX;
     state.bomb.velocity.y = deltaY;
     setInfo(deltaX, deltaY);
-    // console.log(deltaX, deltaY);
-    draw();
+    // draw();
   }
 });
-
-window.addEventListener("mouseup", function (e) {
-  if (isDragging) {
-    isDragging = false;
-    this.document.body.style.cursor = "default";
-    throwBomb();
-  }
-});
+// window.addEventListener("mouseup", function (e) {
+//   console.log("mouse up ");
+// });
 window.addEventListener("resize", () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;

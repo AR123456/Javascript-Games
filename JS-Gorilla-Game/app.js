ecc374db66be9f0f8a780c7af5307dc5a2462bbb
AR123456 <coloradoz99@gmail.com>
2025-12-10 05:38:27 -0500

    draw gorilla player

--- JS-Gorilla-Game/app.js
+++ JS-Gorilla-Game/app.js
@@ -265,6 +265,23 @@ function drawBuildings() {
     }
   });
 }
+function drawGorilla(player) {
+  //  takes in player one or two
+  ctx.save();
+  // building at index 1 or second to last index
+  const building =
+    player === 1 ? state.buildings.at(1) : state.buildings.at(-2);
+  ctx.translate(building.x + building.width / 2, building.height);
+  // this function paints the body of the gorilla as a single path
+  // can use isPointInPath canvas method
+  drawGorillaBody();
+  drawGorillaLeftArm(player);
+  drawGorillaRightArm(player);
+  drawGorillaFace(player);
+  // make one gorilla look a little different
+  drawGorillaThoughtBubbles(player);
+  ctx.restore();
+}
 function drawGorillaBody() {
   ctx.fillStyle = "black";
   ctx.beginPath();
@@ -668,45 +685,6 @@ function checkBuildingHit() {
     }
   }
 }
-
-function drawGorilla(player) {
-  //  takes in player one or two
-  ctx.save();
-  // building at index 1 or second to last index
-  const building =
-    player === 1 ? state.buildings.at(1) : state.buildings.at(-2);
-  ctx.translate(building.x + building.width / 2, building.height);
-  // this function paints the body of the gorilla as a single path
-  // can use isPointInPath canvas method
-  drawGorillaBody();
-  drawGorillaLeftArm(player);
-  drawGorillaRightArm(player);
-  drawGorillaFace(player);
-  // make one gorilla look a little different
-  drawGorillaThoughtBubbles(player);
-  ctx.restore();
-}
-
-function drawBuildingsWithBlastHoles() {
-  ctx.save();
-  state.blastHoles.forEach((blastHole) => {
-    ctx.beginPath();
-    // part of path clockwise behavior
-    ctx.rect(
-      0,
-      0,
-      window.innerWidth / state.scale,
-      window.innerHeight / state.scale
-    );
-    // arc default is to draw clockwise, 6th param "true" makes it go counterclockwise
-    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
-    ctx.arc(blastHole.x, blastHole.y, blastHoleRadius, 0, 2 * Math.PI, true);
-    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/clip
-    ctx.clip();
-  });
-  drawBuildings();
-  ctx.restore();
-}
 function checkGorillaHit() {
   const enemyPlayer = state.currentPlayer === 1 ? 2 : 1;
   // which building is enemy on
@@ -735,6 +713,28 @@ function checkGorillaHit() {
   //  sending this to the animate function
   return hit;
 }
+
+function drawBuildingsWithBlastHoles() {
+  ctx.save();
+  state.blastHoles.forEach((blastHole) => {
+    ctx.beginPath();
+    // part of path clockwise behavior
+    ctx.rect(
+      0,
+      0,
+      window.innerWidth / state.scale,
+      window.innerHeight / state.scale
+    );
+    // arc default is to draw clockwise, 6th param "true" makes it go counterclockwise
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
+    ctx.arc(blastHole.x, blastHole.y, blastHoleRadius, 0, 2 * Math.PI, true);
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/clip
+    ctx.clip();
+  });
+  drawBuildings();
+  ctx.restore();
+}
+
 function announceWinner() {
   winnerDOM.innerText = `Player ${state.currentPlayer}`;
   //TODO  bug with game win

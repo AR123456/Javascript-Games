31e572c6f0b7fe97dffbdf0a9c198aaa19294a2c
AR123456 <coloradoz99@gmail.com>
2025-12-20 07:34:07 -0500

    broken re size

--- JS-Gorilla-Game/app.js
+++ JS-Gorilla-Game/app.js
@@ -169,8 +169,11 @@ window.addEventListener("resize", () => {
   // account for zooming by user
   canvas.width = window.innerWidth * window.devicePixelRatio;
   canvas.height = window.innerHeight * window.devicePixelRatio;
+  canvas.style.width = window.innerWidth + "px";
+  canvas.style.height = window.innerHeight + "px";
   calculateScaleAndShift();
   initializeBombPosition();
+  initializeWindmillPosition();
   draw();
 });
 function initializeBombPosition() {
